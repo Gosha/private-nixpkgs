098911a4a0cf9c6e2cdf1f8f3ca7732f44530cd9
Georgij Zacharov <gosha.za@gmail.com>
2020-06-28 23:08:08 +0200

    pyanidb: Add withXattr flag

--- pyanidb/default.nix
+++ pyanidb/default.nix
@@ -1,7 +1,12 @@
 # { pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz) {};
-{ pkgs ? import <nixpkgs> { }
-, buildPythonPackage ? pkgs.python3Packages.buildPythonPackage
-, fetchFromGitHub ? pkgs.fetchFromGitHub, stdenv ? pkgs.stdenv }:
+{ pkgs ? import <nixpkgs> { }, pythonPackages ? pkgs.python3Packages
+, buildPythonPackage ? pythonPackages.buildPythonPackage
+, fetchFromGitHub ? pkgs.fetchFromGitHub, stdenv ? pkgs.stdenv
+
+  # Flags
+, withXattr ? true
+
+}:
 buildPythonPackage {
   pname = "pyanidb";
   version = "0.2.1";
@@ -16,6 +21,8 @@ buildPythonPackage {
     rev = "b9520212aa037da5344117779817337a1ad918f0";
     sha256 = "17inn816llqwp6qhm3d1m50v0xhalvq9vdnd8i76kjzz0ds6r8sm";
   };
+  propagatedBuildInputs = [ ]
+    ++ pkgs.lib.optional withXattr pythonPackages.xattr;
   meta = with stdenv.lib; {
     homepage = "https://github.com/xyzz/pyanidb";
     description =
