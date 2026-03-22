f78c5b4b05c4b9d02eec500debbd6d6a3599d93f
Georgij Zacharov <gosha.za@gmail.com>
2026-03-22 19:28:19 +0100

    Update pyanidb for nixos-25.11

--- pyanidb/default.nix
+++ pyanidb/default.nix
@@ -1,29 +1,25 @@
-# { pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz) {};
 { pkgs ? import <nixpkgs> { }, python3Packages ? pkgs.python3Packages
 , buildPythonPackage ? python3Packages.buildPythonPackage
-, fetchFromGitHub ? pkgs.fetchFromGitHub, stdenv ? pkgs.stdenv
+, fetchFromGitHub ? pkgs.fetchFromGitHub, withXattr ? true }:
 
-  # Flags
-, withXattr ? true
-
-}:
 buildPythonPackage {
   pname = "pyanidb";
-  version = "0.2.2-a3";
-  postPatch = ''
-    # Upgrade some constructs to python3
-    2to3 -w .
-  '';
+  version = "0.2.2a3";
+
   src = fetchFromGitHub {
     owner = "Gosha";
     repo = "pyanidb";
     rev = "81eefb1947ddcd620eab08dee9930787e844120d";
     hash = "sha256-1KWnY/LkO5vexndUbm8u7R2+4VZ+3MJkkITbHxFgPAY=";
   };
+
+  format = "setuptools";
+
   propagatedBuildInputs = [ python3Packages.pycryptodome ]
     ++ pkgs.lib.optional withXattr python3Packages.xattr;
+
   meta = with pkgs.lib; {
-    homepage = "https://github.com/xyzz/pyanidb";
+    homepage = "https://github.com/Gosha/pyanidb";
     description =
       "PyAniDB is a client for AniDB's UDP API. (http://anidb.net/)";
     license = licenses.gpl2;
