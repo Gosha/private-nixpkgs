f76dbecd1e8eb63cdc4444ca5e02a43c5ebf0038
Georgij Zacharov <gosha.za@gmail.com>
2026-09-17 16:48:23 +0200

    Update wolt-cli 2.4.1

--- wolt-cli/default.nix
+++ wolt-cli/default.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "wolt-cli";
-  version = "2.2.0";
+  version = "2.4.1";
 
   src = fetchFromGitHub {
     owner = "mekedron";
     repo = "wolt-cli";
     rev = "v${version}";
-    hash = "sha256-uRVdDEplxh4u2DDizJmobRh9DExrNh37SYLQvwMtXoo=";
+    hash = "sha256-JrtdcJe63OQvuPG9pMv2uk7VS+8thK410V8nrZlg/HA=";
   };
 
-  vendorHash = "sha256-/XM/BF3YIIZzp+it92fv1Z9QBFIQYI75tnqnAE5KsJo=";
+  vendorHash = "sha256-7reCguzo+A8m9kPV9k1Qh1oqbaYUEzXwDzaOXfWtsT0=";
 
   subPackages = [
     "cmd/wolt"
