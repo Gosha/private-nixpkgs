b6ea111fb9b1b780a5b120384678b3e628ef5bc0
Georgij Zacharov <gosha.za@gmail.com>
2026-09-19 14:46:48 +0200

    Update ikea-dirigera 2.7.1

--- ikea-dirigera/module.nix
+++ ikea-dirigera/module.nix
@@ -1,20 +1,28 @@
-{ stdenv, pkgs, fetchFromGitHub, buildHomeAssistantComponent
-#, dirigera_platform
-}:
+{ lib, home-assistant, fetchFromGitHub, buildHomeAssistantComponent }:
 
 buildHomeAssistantComponent rec {
-
   owner = "sanjoyg";
   domain = "dirigera_platform";
-  version = "2.6.8";
+  version = "2.7.1";
 
   src = fetchFromGitHub {
     owner = "sanjoyg";
     repo = "dirigera_platform";
     rev = version;
-    sha256 = "sha256-5eTHJsE5Jof5WSZFkf8/1UQafpgxpGTPuDWQMENgAG0=";
+    sha256 = "sha256-N4H07CmIEqUqv1VkLlL1f924TvZ4Cb4IuVKlRYJA9CM=";
   };
 
-  # propagatedBuildInputs = [ dirigera_platform ];
+  dependencies = [ home-assistant.python3Packages.dirigera ];
 
+  postPatch = ''
+    substituteInPlace custom_components/dirigera_platform/manifest.json \
+      --replace-fail 'dirigera==1.2.6' 'dirigera==${home-assistant.python3Packages.dirigera.version}' \
+      --replace-fail '"version": "0.0.1"' '"version": "${version}"'
+  '';
+
+  meta = {
+    description = "Home Assistant integration for IKEA Dirigera hubs";
+    homepage = "https://github.com/sanjoyg/dirigera_platform";
+    license = lib.licenses.mit;
+  };
 }
